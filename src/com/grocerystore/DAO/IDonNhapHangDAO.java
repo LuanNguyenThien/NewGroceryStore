818ab6073091476d4bbe0e9e6de7f2ceb3c7db25
Le Quang Lam <work.lamsun@gmail.com>
2024-04-05 20:08:45 +0700

    add IChiTietDonNhapHangDAO, IDonNhapHangDao

---
+++ src/com/grocerystore/DAO/IDonNhapHangDAO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.grocerystore.DAO;
+
+import com.grocerystore.model.DonNhapHang;
+import java.util.List;
+
+/**
+ *
+ * @author A
+ */
+public interface IDonNhapHangDAO {
+    Boolean ThemDNH(DonNhapHang donNhapHang);
+    List<DonNhapHang> GetDSDNH(); //lay trang thai chua nhap
+    Boolean XacNhanDNH(String MaDNH);
+    Boolean XoaDNH(String MaDNH);
+}
